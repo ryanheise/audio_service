8ef876afbb3a494ab3c2aeca8234120d8b9cbcc9
Eiichiro Adachi <addie9000@users.noreply.github.com>
2020-03-26 00:43:51 +0900

    Support skip forward backward in command center (iOS). (#196)
    * Support skip forward and backward in command center.
    
    * Add fast forward and rewind interval parameter.

--- ios/Classes/AudioServicePlugin.m
+++ ios/Classes/AudioServicePlugin.m
@@ -115,11 +115,21 @@ static MPMediaItemArtwork* artwork = nil;
     if (@available(iOS 9.1, *)) {
       [commandCenter.changePlaybackPositionCommand addTarget:self action:@selector(changePlaybackPosition:)];
     }
+    // Skipping
+    NSNumber *skipForwardInterval = [call.arguments objectForKey:@"fastForwardInterval"];
+    NSNumber *skipBackwardInterval = [call.arguments objectForKey:@"rewindInterval"];
+    if (skipForwardInterval > 0) {
+      [commandCenter.skipForwardCommand setEnabled:YES];
+      [commandCenter.skipForwardCommand addTarget: self action:@selector(skipForward:)];
+      commandCenter.skipForwardCommand.preferredIntervals = @[skipForwardInterval];
+    }
+    if (skipBackwardInterval > 0) {
+      [commandCenter.skipBackwardCommand setEnabled:YES];
+      [commandCenter.skipBackwardCommand addTarget: self action:@selector(skipBackward:)];
+      commandCenter.skipBackwardCommand.preferredIntervals = @[skipBackwardInterval];
+    }
 
     // TODO: enable more commands
-    // Skipping
-    [commandCenter.skipForwardCommand setEnabled:NO];
-    [commandCenter.skipBackwardCommand setEnabled:NO];
     // Seeking
     [commandCenter.seekForwardCommand setEnabled:NO];
     [commandCenter.seekBackwardCommand setEnabled:NO];
@@ -161,6 +171,11 @@ static MPMediaItemArtwork* artwork = nil;
     if (@available(iOS 9.1, *)) {
       [commandCenter.changePlaybackPositionCommand removeTarget:nil];
     }
+    // Skipping
+    [commandCenter.skipForwardCommand setEnabled:NO];
+    [commandCenter.skipBackwardCommand setEnabled:NO];
+    [commandCenter.skipForwardCommand removeTarget:nil];
+    [commandCenter.skipBackwardCommand removeTarget:nil];
     result(@YES);
   } else if ([@"isRunning" isEqualToString:call.method]) {
     if (_running) {
@@ -339,4 +354,15 @@ static MPMediaItemArtwork* artwork = nil;
   return MPRemoteCommandHandlerStatusSuccess;
 }
 
+- (MPRemoteCommandHandlerStatus) skipForward: (MPRemoteCommandEvent *) event {
+  NSLog(@"skipForward");
+  [backgroundChannel invokeMethod:@"onFastForward" arguments:nil];
+  return MPRemoteCommandHandlerStatusSuccess;
+}
+
+- (MPRemoteCommandHandlerStatus) skipBackward: (MPRemoteCommandEvent *) event {
+  NSLog(@"skipBackward");
+  [backgroundChannel invokeMethod:@"onRewind" arguments:nil];
+  return MPRemoteCommandHandlerStatusSuccess;
+}
 @end
