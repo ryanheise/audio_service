cbd9f8c166b724234944bb0a7f32de8604d021a9
Family Nasr <mohammadne@mail.ru>
2020-04-10 15:43:12 +0430

    new main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,34 +4,10 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:audio_service/audio_service.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:rxdart/rxdart.dart';
 
-MediaControl playControl = MediaControl(
-  androidIcon: 'drawable/ic_action_play_arrow',
-  label: 'Play',
-  action: MediaAction.play,
-);
-MediaControl pauseControl = MediaControl(
-  androidIcon: 'drawable/ic_action_pause',
-  label: 'Pause',
-  action: MediaAction.pause,
-);
-MediaControl skipToNextControl = MediaControl(
-  androidIcon: 'drawable/ic_action_skip_next',
-  label: 'Next',
-  action: MediaAction.skipToNext,
-);
-MediaControl skipToPreviousControl = MediaControl(
-  androidIcon: 'drawable/ic_action_skip_previous',
-  label: 'Previous',
-  action: MediaAction.skipToPrevious,
-);
-MediaControl stopControl = MediaControl(
-  androidIcon: 'drawable/ic_action_stop',
-  label: 'Stop',
-  action: MediaAction.stop,
-);
+import 'audio_player.dart';
+import 'audio_service.dart';
 
 void main() => runApp(new MyApp());
 
@@ -132,7 +108,22 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
                       ),
                     if (mediaItem?.title != null) Text(mediaItem.title),
                     if (basicState == BasicPlaybackState.none) ...[
-                      audioPlayerButton(),
+                      RaisedButton(
+                        child: Text("audio service"),
+                        onPressed: () {
+                          AudioService.start(
+                            backgroundTaskEntrypoint: () async =>
+                                AudioServiceBackground.run(
+                              () => AudioServiceTask(JustAudioPlayer()),
+                            ),
+                            androidNotificationChannelName:
+                                'Audio Service Demo',
+                            notificationColor: 0xFF2196f3,
+                            androidNotificationIcon: 'mipmap/ic_launcher',
+                            enableQueue: true,
+                          );
+                        },
+                      )
                     ] else
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -167,9 +158,10 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
                             print(AudioService.queue.toString());
                           }),
                       RaisedButton(
-                          child: Text("add item"),
-                          onPressed: () {
-                            AudioService.addQueueItem(MediaItem(
+                        child: Text("add item"),
+                        onPressed: () {
+                          AudioService.addQueueItem(
+                            MediaItem(
                               id: "https://dl.irangfx.com/gerama/albums/ali-jafari-pouyan/1-6/music/ali-jafari-pouyan-1-6-2019.mp3",
                               album: "Gerama album",
                               title: "Gerama title",
@@ -177,8 +169,10 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
                               duration: 444000,
                               artUri:
                                   "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg",
-                            ));
-                          }),
+                            ),
+                          );
+                        },
+                      ),
                     ],
                   ],
                 );
@@ -189,25 +183,6 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
       ),
     );
   }
-
-  RaisedButton audioPlayerButton() => startButton(
-        'AudioPlayer',
-        () {
-          AudioService.start(
-            backgroundTaskEntrypoint: _audioPlayerTaskEntrypoint,
-            androidNotificationChannelName: 'Audio Service Demo',
-            notificationColor: 0xFF2196f3,
-            androidNotificationIcon: 'mipmap/ic_launcher',
-            enableQueue: true,
-          );
-        },
-      );
-
-  RaisedButton startButton(String label, VoidCallback onPressed) =>
-      RaisedButton(
-        child: Text(label),
-        onPressed: onPressed,
-      );
 
   IconButton playButton() => IconButton(
         icon: Icon(Icons.play_arrow),
@@ -273,202 +248,4 @@ class ScreenState {
   final PlaybackState playbackState;
 
   ScreenState(this.queue, this.mediaItem, this.playbackState);
-}
-
-void _audioPlayerTaskEntrypoint() async {
-  AudioServiceBackground.run(() => AudioPlayerTask());
-}
-
-class AudioPlayerTask extends BackgroundAudioTask {
-  final _queue = <MediaItem>[
-    MediaItem(
-      id: "https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3",
-      album: "Science Friday",
-      title: "A Salute To Head-Scratching Science",
-      artist: "Science Friday and WNYC Studios",
-      duration: 5739820,
-      artUri:
-          "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg",
-    ),
-    MediaItem(
-      id: "https://s3.amazonaws.com/scifri-segments/scifri201711241.mp3",
-      album: "Science Friday",
-      title: "From Cat Rheology To Operatic Incompetence",
-      artist: "Science Friday and WNYC Studios",
-      duration: 2856950,
-      artUri:
-          "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg",
-    ),
-  ];
-  int _queueIndex = -1;
-  AudioPlayer _audioPlayer = new AudioPlayer();
-  Completer _completer = Completer();
-  BasicPlaybackState _skipState;
-  bool _playing;
-
-  bool get hasNext => _queueIndex + 1 < _queue.length;
-
-  bool get hasPrevious => _queueIndex > 0;
-
-  MediaItem get mediaItem => _queue[_queueIndex];
-
-  BasicPlaybackState _eventToBasicState(AudioPlaybackEvent event) {
-    if (event.buffering) {
-      return BasicPlaybackState.buffering;
-    } else {
-      switch (event.state) {
-        case AudioPlaybackState.none:
-          return BasicPlaybackState.none;
-        case AudioPlaybackState.stopped:
-          return BasicPlaybackState.stopped;
-        case AudioPlaybackState.paused:
-          return BasicPlaybackState.paused;
-        case AudioPlaybackState.playing:
-          return BasicPlaybackState.playing;
-        case AudioPlaybackState.connecting:
-          return _skipState ?? BasicPlaybackState.connecting;
-        case AudioPlaybackState.completed:
-          return BasicPlaybackState.stopped;
-        default:
-          throw Exception("Illegal state");
-      }
-    }
-  }
-
-  @override
-  Future<void> onStart() async {
-    var playerStateSubscription = _audioPlayer.playbackStateStream
-        .where((state) => state == AudioPlaybackState.completed)
-        .listen((state) {
-      _handlePlaybackCompleted();
-    });
-    var eventSubscription = _audioPlayer.playbackEventStream.listen((event) {
-      final state = _eventToBasicState(event);
-      if (state != BasicPlaybackState.stopped) {
-        _setState(
-          state: state,
-          position: event.position.inMilliseconds,
-        );
-      }
-    });
-
-    AudioServiceBackground.setQueue(_queue);
-    await onSkipToNext();
-    await _completer.future;
-    playerStateSubscription.cancel();
-    eventSubscription.cancel();
-  }
-
-  void _handlePlaybackCompleted() {
-    if (hasNext) {
-      onSkipToNext();
-    } else {
-      onStop();
-    }
-  }
-
-  void playPause() {
-    if (_skipState == null) {
-      if (AudioServiceBackground.state.basicState ==
-          BasicPlaybackState.playing) {
-        _playing = true;
-        _audioPlayer.play();
-      } else {
-        _playing = false;
-        _audioPlayer.pause();
-      }
-    }
-  }
-
-  @override
-  void onAddQueueItem(MediaItem mediaItem) {
-    a(mediaItem);
-  }
-
-  Future a(MediaItem media) async {
-    await _audioPlayer.stop();
-    List b = _queue;
-    b.add(media);
-    AudioServiceBackground.setQueue(b);
-    onSkipToNext();
-  }
-
-  @override
-  Future<void> onSkipToNext() => _skip(1);
-
-  @override
-  Future<void> onSkipToPrevious() => _skip(-1);
-
-  Future<void> _skip(int offset) async {
-    final newPos = _queueIndex + offset;
-    if (!(newPos >= 0 && newPos < _queue.length)) return;
-    if (_playing == null) {
-      // First time, we want to start playing
-      _playing = true;
-    } else if (_playing) {
-      // Stop current item
-      await _audioPlayer.stop();
-    }
-    // Load next item
-    _queueIndex = newPos;
-    AudioServiceBackground.setMediaItem(mediaItem);
-    _skipState = offset > 0
-        ? BasicPlaybackState.skippingToNext
-        : BasicPlaybackState.skippingToPrevious;
-    await _audioPlayer.setUrl(mediaItem.id);
-    _skipState = null;
-    // Resume playback if we were playing
-    if (_playing) {
-      _audioPlayer.play();
-    } else {
-      _setState(state: BasicPlaybackState.paused);
-    }
-  }
-
-  @override
-  void onSeekTo(int position) {
-    _audioPlayer.seek(Duration(milliseconds: position));
-  }
-
-  @override
-  void onClick(MediaButton button) {
-    playPause();
-  }
-
-  @override
-  void onStop() {
-    _audioPlayer.stop();
-    _setState(state: BasicPlaybackState.stopped);
-    _completer.complete();
-  }
-
-  void _setState({@required BasicPlaybackState state, int position}) {
-    if (position == null) {
-      position = _audioPlayer.playbackEvent.position.inMilliseconds;
-    }
-    AudioServiceBackground.setState(
-      controls: getControls(state),
-      systemActions: [MediaAction.seekTo],
-      basicState: state,
-      position: position,
-    );
-  }
-
-  List<MediaControl> getControls(BasicPlaybackState state) {
-    if (_playing) {
-      return [
-        skipToPreviousControl,
-        pauseControl,
-        stopControl,
-        skipToNextControl
-      ];
-    } else {
-      return [
-        skipToPreviousControl,
-        playControl,
-        stopControl,
-        skipToNextControl
-      ];
-    }
-  }
 }
