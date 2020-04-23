40917fc484317bdee5e63a21dc563fb9d76a15a1
Amarjit Singh <b.singh.amarjit@gmail.com>
2020-04-23 07:26:49 -0700

    Add Artist to nowPlayingInfo

--- ios/Classes/AudioServicePlugin.m
+++ ios/Classes/AudioServicePlugin.m
@@ -314,6 +314,7 @@ static MPMediaItemArtwork* artwork = nil;
   if (mediaItem) {
     nowPlayingInfo[MPMediaItemPropertyTitle] = mediaItem[@"title"];
     nowPlayingInfo[MPMediaItemPropertyAlbumTitle] = mediaItem[@"album"];
+    nowPlayingInfo[MPMediaItemPropertyArtist] = mediaItem[@"artist"];
     if (mediaItem[@"duration"] != [NSNull null]) {
       nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = [NSNumber numberWithLongLong: ([mediaItem[@"duration"] longLongValue] / 1000)];
     }
