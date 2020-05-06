d1cad72993c9db0a49ff64114b9aa24e17aff436
Ryan Heise <ryan@ryanheise.com>
2020-05-06 20:25:24 +1000

    Remove memory leak workaround.

--- example/android/app/src/main/java/com/ryanheise/audioserviceexample/MainActivity.java
+++ example/android/app/src/main/java/com/ryanheise/audioserviceexample/MainActivity.java
@@ -1,22 +1,6 @@
 package com.ryanheise.audioserviceexample;
 
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.embedding.engine.dart.DartExecutor;
-import android.content.Context;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-	/** This is a temporary workaround to avoid a memory leak in the Flutter framework */
-	@Override
-	public FlutterEngine provideFlutterEngine(Context context) {
-		// Instantiate a FlutterEngine.
-		FlutterEngine flutterEngine = new FlutterEngine(context.getApplicationContext());
-
-		// Start executing Dart code to pre-warm the FlutterEngine.
-		flutterEngine.getDartExecutor().executeDartEntrypoint(
-			DartExecutor.DartEntrypoint.createDefault()
-		);
-
-		return flutterEngine;
-	}
 }
