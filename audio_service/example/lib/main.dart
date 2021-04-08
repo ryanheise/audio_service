84d47c2a2707c80a715ac686e896e67b360dc007
nt4f04uNd <nt4f04uNd@gmail.com>
2021-04-08 19:04:30 +0300

    changed some quotes back

--- audio_service/example/lib/main.dart
+++ audio_service/example/lib/main.dart
@@ -790,18 +790,18 @@ class MediaLibrary {
     albumsRootId: [
       MediaItem(
         id: 'https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3',
-        album: 'cience Friday',
-        title: 'A Salute To Head-Scratching Science',
-        artist: 'Science Friday and WNYC Studios',
+        album: "Science Friday",
+        title: "A Salute To Head-Scratching Science",
+        artist: "Science Friday and WNYC Studios",
         duration: const Duration(milliseconds: 5739820),
         artUri: Uri.parse(
             'https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg'),
       ),
       MediaItem(
         id: 'https://s3.amazonaws.com/scifri-segments/scifri201711241.mp3',
-        album: 'Science Friday',
-        title: 'From Cat Rheology To Operatic Incompetence',
-        artist: 'Science Friday and WNYC Studios',
+        album: "Science Friday",
+        title: "From Cat Rheology To Operatic Incompetence",
+        artist: "Science Friday and WNYC Studios",
         duration: const Duration(milliseconds: 2856950),
         artUri: Uri.parse(
             'https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg'),
