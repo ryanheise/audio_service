1024a77444f0f6d7c433f5491b5ee0556203cd81
nt4f04uNd <nt4f04uNd@gmail.com>
2021-06-08 03:24:04 +0300

    remove unused import

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServicePlugin.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServicePlugin.java
@@ -29,7 +29,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugin.common.MethodCall;
