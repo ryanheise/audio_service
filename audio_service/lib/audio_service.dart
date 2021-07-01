aa7e8bd36080b13224f6d24df8bb34e525959e5d
nt4f04uNd <nt4f04uNd@gmail.com>
2021-07-01 14:26:56 +0300

    +

--- audio_service/lib/audio_service.dart
+++ audio_service/lib/audio_service.dart
@@ -28,111 +28,79 @@ enum MediaButton {
 enum MediaAction {
   /// Stop playing audio.
   stop,
-  // 0
 
   /// Pause audio.
   pause,
-  // 1
 
   /// Play or resume audio.
   play,
-  // 2
 
   /// Rewind.
   rewind,
-  // 3
 
   /// Skip to the previous media item.
   skipToPrevious,
-  // 4
 
   /// Skip to the next media item.
   skipToNext,
-  // 5
 
   /// Fast forward.
   fastForward,
-  // 6
 
   /// Set a rating for the current media item.
   setRating,
-  // 7
 
   /// Seek within the current media item.
   seek,
-  // 8
 
   /// Toggle between playing and paused.
   playPause,
-  // 9
 
   /// Play a given media item by ID.
   playFromMediaId,
-  // 10
 
   /// Play media from a search.
   playFromSearch,
-  // 11
 
   /// Skip to a queue item.
   skipToQueueItem,
-  // 12
 
   /// Play media from a URI.
   playFromUri,
-  // 13
 
   /// Prepare media for playback.
   prepare,
-  // 14
 
   /// Prepare media for playback by ID.
   prepareFromMediaId,
-  // 15
 
   /// Prepare media for playback from a search.
   prepareFromSearch,
-  // 16
 
   /// Prepare media for playback from a URI.
   prepareFromUri,
-  // 17
 
   /// Set the repeat mode.
   setRepeatMode,
-  // 18
+
+  /// Was depreceated in Android.
+  // ignore: unused_field
+  _setShuffleModeEnabled,
 
   /// Set captioning enabled.
   setCaptioningEnabled,
-  // 19
-
-  // On Android corresponds to the deprecated `SET_SHUFFLE_MODE_ENABLED`
-  _unused_2,
-  // 20
 
   /// Set the shuffle mode.
   setShuffleMode,
-  // 21
 
-  /// Set playback speed.
+  /// Set speed.
   setSpeed,
-  // 22
 
   /// Seek backwards continuously.
   seekBackward,
-  // 23
 
   /// Seek forwards continuously.
   seekForward,
-  // 24
-
-  /// Unused.
-  @Deprecated("Doesn't do anything. Will be removed in the next release")
-  unused_1,
-
-  /// Unused.
-  @Deprecated("Doesn't do anything. Will be removed in the next release")
-  unused_2,
 }
 
 /// The states of audio processing.
