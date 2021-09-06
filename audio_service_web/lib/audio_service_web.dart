af722395bf208bb7725c07821eac7b67477619c5
nt4f04uNd <nt4f04uNd@gmail.com>
2021-09-06 16:46:39 +0300

    Cleanup web implementation

--- audio_service_web/lib/audio_service_web.dart
+++ audio_service_web/lib/audio_service_web.dart
@@ -19,77 +19,69 @@ class AudioServiceWeb extends AudioServicePlatform {
 
   @override
   Future<void> setState(SetStateRequest request) async {
-    print('Setting state');
     final session = html.window.navigator.mediaSession!;
     for (final control in request.state.controls) {
-      try {
-        switch (control.action) {
-          case MediaActionMessage.play:
-            session.setActionHandler(
-              'play',
-              () => handlerCallbacks?.play(const PlayRequest()),
-            );
-            break;
-          case MediaActionMessage.pause:
-            session.setActionHandler(
-              'pause',
-              () => handlerCallbacks?.pause(const PauseRequest()),
-            );
-            break;
-          case MediaActionMessage.skipToPrevious:
-            session.setActionHandler(
-              'previoustrack',
-              () => handlerCallbacks
-                  ?.skipToPrevious(const SkipToPreviousRequest()),
-            );
-            break;
-          case MediaActionMessage.skipToNext:
-            session.setActionHandler(
-              'nexttrack',
-              () => handlerCallbacks?.skipToNext(const SkipToNextRequest()),
-            );
-            break;
-          // The naming convention here is a bit odd but seekbackward seems more
-          // analagous to rewind than seekBackward
-          case MediaActionMessage.rewind:
-            session.setActionHandler(
-              'seekbackward',
-              () => handlerCallbacks?.rewind(const RewindRequest()),
-            );
-            break;
-          case MediaActionMessage.fastForward:
-            session.setActionHandler(
-              'seekforward',
-              () => handlerCallbacks?.fastForward(const FastForwardRequest()),
-            );
-            break;
-          case MediaActionMessage.stop:
-            session.setActionHandler(
-              'stop',
-              () => handlerCallbacks?.stop(const StopRequest()),
-            );
-            break;
-          default:
-            // no-op
-            break;
-        }
-      } catch (e) {
-        // TODO: handle this somehow?
+      switch (control.action) {
+        case MediaActionMessage.play:
+          session.setActionHandler(
+            'play',
+            () => handlerCallbacks?.play(const PlayRequest()),
+          );
+          break;
+        case MediaActionMessage.pause:
+          session.setActionHandler(
+            'pause',
+            () => handlerCallbacks?.pause(const PauseRequest()),
+          );
+          break;
+        case MediaActionMessage.skipToPrevious:
+          session.setActionHandler(
+            'previoustrack',
+            () =>
+                handlerCallbacks?.skipToPrevious(const SkipToPreviousRequest()),
+          );
+          break;
+        case MediaActionMessage.skipToNext:
+          session.setActionHandler(
+            'nexttrack',
+            () => handlerCallbacks?.skipToNext(const SkipToNextRequest()),
+          );
+          break;
+        // The naming convention here is a bit odd but seekbackward seems more
+        // analagous to rewind than seekBackward
+        case MediaActionMessage.rewind:
+          session.setActionHandler(
+            'seekbackward',
+            () => handlerCallbacks?.rewind(const RewindRequest()),
+          );
+          break;
+        case MediaActionMessage.fastForward:
+          session.setActionHandler(
+            'seekforward',
+            () => handlerCallbacks?.fastForward(const FastForwardRequest()),
+          );
+          break;
+        case MediaActionMessage.stop:
+          session.setActionHandler(
+            'stop',
+            () => handlerCallbacks?.stop(const StopRequest()),
+          );
+          break;
+        default:
+          // no-op
+          break;
       }
+
       for (final message in request.state.systemActions) {
         switch (message) {
           case MediaActionMessage.seek:
-            try {
-              setActionHandler('seekto', js.allowInterop((ActionResult ev) {
-                // Chrome uses seconds for whatever reason
-                handlerCallbacks?.seek(SeekRequest(
-                    position: Duration(
-                  milliseconds: (ev.seekTime * 1000).round(),
-                )));
-              }));
-            } catch (e) {
-              // TODO: handle this somehow?
-            }
+            setActionHandler('seekto', js.allowInterop((ActionResult ev) {
+              // Chrome uses seconds for whatever reason
+              handlerCallbacks?.seek(SeekRequest(
+                  position: Duration(
+                milliseconds: (ev.seekTime * 1000).round(),
+              )));
+            }));
             break;
           default:
             // no-op
@@ -97,28 +89,29 @@ class AudioServiceWeb extends AudioServicePlatform {
         }
       }
 
-      try {
-        // Dart also doesn't expose setPositionState
-        if (mediaItem != null) {
-          //print(
-          //    'Setting positionState Duration(${mediaItem!.duration?.inSeconds}), PlaybackRate(${args[6] ?? 1.0}), Position(${Duration(milliseconds: args[4])?.inSeconds})');
-
-          // Chrome looks for seconds for some reason
-          setPositionState(PositionState(
-            duration: (mediaItem!.duration?.inMilliseconds ?? 0) / 1000,
-            playbackRate: request.state.speed,
-            position: request.state.updatePosition.inMilliseconds / 1000,
-          ));
-        }
-      } catch (e) {
-        print(e);
+      // Update the position
+      //
+      // Factor out invalid states according to
+      // https://developer.mozilla.org/en-US/docs/Web/API/MediaSession/setPositionState#exceptions
+      var duration = Duration.zero;
+      var position = request.state.updatePosition;
+      if (mediaItem != null) {
+        duration = mediaItem!.duration ?? Duration.zero;
       }
+      if (position > duration) {
+        position = duration;
+      }
+      setPositionState(PositionState(
+        duration: duration.inSeconds.toDouble(),
+        playbackRate: request.state.speed,
+        position: position.inSeconds.toDouble(),
+      ));
     }
   }
 
   @override
   Future<void> setQueue(SetQueueRequest request) async {
-    //no-op there is not a queue concept on the web
+    // no-op as there is not a queue concept on the web
   }
 
   @override
@@ -126,23 +119,17 @@ class AudioServiceWeb extends AudioServicePlatform {
     mediaItem = request.mediaItem;
     final artUri = mediaItem!.artUri;
 
-    print('setting media item!');
-
-    try {
-      metadata = html.MediaMetadata(<String, dynamic>{
-        'album': mediaItem!.album,
-        'title': mediaItem!.title,
-        'artist': mediaItem!.artist,
-        'artwork': [
-          {
-            'src': artUri,
-            'sizes': '512x512',
-          }
-        ],
-      });
-    } catch (e) {
-      print('Metadata failed $e');
-    }
+    metadata = html.MediaMetadata(<String, dynamic>{
+      'album': mediaItem!.album,
+      'title': mediaItem!.title,
+      'artist': mediaItem!.artist,
+      'artwork': [
+        {
+          'src': artUri,
+          'sizes': '512x512',
+        }
+      ],
+    });
   }
 
   @override
