0a49521b67e08b229b391644f4942e867689868f
nt4f04uNd <nt4f04uNd@gmail.com>
2021-09-09 15:53:23 +0300

    Return some catch statments

--- audio_service_web/lib/audio_service_web.dart
+++ audio_service_web/lib/audio_service_web.dart
@@ -12,7 +12,7 @@ class AudioServiceWeb extends AudioServicePlatform {
   }
 
   bool _loggedNotSupported = false;
-  bool checkMediaSessionSupported() {
+  bool _checkMediaSessionSupported() {
     final supported = js.context.hasProperty('MediaSession');
     if (!supported && !_loggedNotSupported) {
       _loggedNotSupported = true;
@@ -27,67 +27,75 @@ class AudioServiceWeb extends AudioServicePlatform {
 
   @override
   Future<void> configure(ConfigureRequest request) async {
-    checkMediaSessionSupported();
+    _checkMediaSessionSupported();
   }
 
   @override
   Future<void> setState(SetStateRequest request) async {
-    if (!checkMediaSessionSupported()) {
+    if (!_checkMediaSessionSupported()) {
       return;
     }
-    final session = html.window.navigator.mediaSession!;
-    for (final control in request.state.controls) {
-      switch (control.action) {
-        case MediaActionMessage.play:
-          session.setActionHandler(
-            'play',
-            () => handlerCallbacks?.play(const PlayRequest()),
-          );
-          break;
-        case MediaActionMessage.pause:
-          session.setActionHandler(
-            'pause',
-            () => handlerCallbacks?.pause(const PauseRequest()),
-          );
-          break;
-        case MediaActionMessage.skipToPrevious:
-          session.setActionHandler(
-            'previoustrack',
-            () =>
-                handlerCallbacks?.skipToPrevious(const SkipToPreviousRequest()),
-          );
-          break;
-        case MediaActionMessage.skipToNext:
-          session.setActionHandler(
-            'nexttrack',
-            () => handlerCallbacks?.skipToNext(const SkipToNextRequest()),
-          );
-          break;
-        // The naming convention here is a bit odd but seekbackward seems more
-        // analagous to rewind than seekBackward
-        case MediaActionMessage.rewind:
-          session.setActionHandler(
-            'seekbackward',
-            () => handlerCallbacks?.rewind(const RewindRequest()),
-          );
-          break;
-        case MediaActionMessage.fastForward:
-          session.setActionHandler(
-            'seekforward',
-            () => handlerCallbacks?.fastForward(const FastForwardRequest()),
-          );
-          break;
-        case MediaActionMessage.stop:
-          session.setActionHandler(
-            'stop',
-            () => handlerCallbacks?.stop(const StopRequest()),
-          );
-          break;
-        default:
-          // no-op
-          break;
-      }
 
+    try {
+      final session = html.window.navigator.mediaSession!;
+      for (final control in request.state.controls) {
+        switch (control.action) {
+          case MediaActionMessage.play:
+            session.setActionHandler(
+              'play',
+              () => handlerCallbacks?.play(const PlayRequest()),
+            );
+            break;
+          case MediaActionMessage.pause:
+            session.setActionHandler(
+              'pause',
+              () => handlerCallbacks?.pause(const PauseRequest()),
+            );
+            break;
+          case MediaActionMessage.skipToPrevious:
+            session.setActionHandler(
+              'previoustrack',
+              () => handlerCallbacks
+                  ?.skipToPrevious(const SkipToPreviousRequest()),
+            );
+            break;
+          case MediaActionMessage.skipToNext:
+            session.setActionHandler(
+              'nexttrack',
+              () => handlerCallbacks?.skipToNext(const SkipToNextRequest()),
+            );
+            break;
+          // The naming convention here is a bit odd but seekbackward seems more
+          // analagous to rewind than seekBackward
+          case MediaActionMessage.rewind:
+            session.setActionHandler(
+              'seekbackward',
+              () => handlerCallbacks?.rewind(const RewindRequest()),
+            );
+            break;
+          case MediaActionMessage.fastForward:
+            session.setActionHandler(
+              'seekforward',
+              () => handlerCallbacks?.fastForward(const FastForwardRequest()),
+            );
+            break;
+          case MediaActionMessage.stop:
+            session.setActionHandler(
+              'stop',
+              () => handlerCallbacks?.stop(const StopRequest()),
+            );
+            break;
+          default:
+            // no-op
+            break;
+        }
+      }
+    } catch (ex) {
+      // In case some browsers don't have `setActionHandler` implemented.
+      print(ex);
+    }
+
+    try {
       for (final message in request.state.systemActions) {
         switch (message) {
           case MediaActionMessage.seek:
@@ -104,7 +112,12 @@ class AudioServiceWeb extends AudioServicePlatform {
             break;
         }
       }
+    } catch (ex) {
+      // In case some browsers don't have `setActionHandler` implemented.
+      print(ex);
+    }
 
+    try {
       // Update the position
       //
       // Factor out invalid states according to
@@ -123,6 +136,9 @@ class AudioServiceWeb extends AudioServicePlatform {
         playbackRate: request.state.speed,
         position: position.inMilliseconds / 1000,
       ));
+    } catch (ex) {
+      // In case some browsers don't have `setPositionState` implemented.
+      print(ex);
     }
   }
 
@@ -133,7 +149,7 @@ class AudioServiceWeb extends AudioServicePlatform {
 
   @override
   Future<void> setMediaItem(SetMediaItemRequest request) async {
-    if (!checkMediaSessionSupported()) {
+    if (!_checkMediaSessionSupported()) {
       return;
     }
     mediaItem = request.mediaItem;
@@ -154,7 +170,7 @@ class AudioServiceWeb extends AudioServicePlatform {
 
   @override
   Future<void> stopService(StopServiceRequest request) async {
-    if (!checkMediaSessionSupported()) {
+    if (!_checkMediaSessionSupported()) {
       return;
     }
     final session = html.window.navigator.mediaSession!;
@@ -164,7 +180,7 @@ class AudioServiceWeb extends AudioServicePlatform {
 
   @override
   void setHandlerCallbacks(AudioHandlerCallbacks callbacks) {
-    if (!checkMediaSessionSupported()) {
+    if (!_checkMediaSessionSupported()) {
       return;
     }
     // Save this here so that we can modify which handlers are set based
