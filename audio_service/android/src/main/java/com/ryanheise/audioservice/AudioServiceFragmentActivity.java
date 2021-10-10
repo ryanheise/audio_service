9fcba8277e92463ff2aa5864adfc416d2bdd3f74
Aurimas Deimantas <aurimas@fertilemind.app>
2021-10-10 11:10:08 +0700

    feat: implement fragment activity for audio service

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceFragmentActivity.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceFragmentActivity.java
@@ -4,12 +4,17 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 
-public class AudioServiceActivity extends FlutterActivity {
+public class AudioServiceFragmentActivity extends FlutterFragmentActivity {
     @Override
     public FlutterEngine provideFlutterEngine(@NonNull Context context) {
         return AudioServicePlugin.getFlutterEngine(context);
+    }
+
+    protected String getCachedEngineId() {
+        AudioServicePlugin.getFlutterEngine(this);
+        return AudioServicePlugin.getFlutterEngineId();
     }
 }
