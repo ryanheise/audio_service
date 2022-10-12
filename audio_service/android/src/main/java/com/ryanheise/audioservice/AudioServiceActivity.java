9822048ee8d36e1169680531bfdf55c5465506af
Prateek Sunal <prtksunal@gmail.com>
2022-10-12 11:02:54 +0530

    Update AudioServiceActivity.java

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
@@ -5,9 +5,10 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 
-public class AudioServiceActivity extends FlutterActivity {
+public class AudioServiceActivity extends FlutterFragmentActivity {
     @Override
     public FlutterEngine provideFlutterEngine(@NonNull Context context) {
         return AudioServicePlugin.getFlutterEngine(context);
