1b8d0e43dba3f00971c689ea08fdac8d3ce3b96d
Ryan Heise <ryan@ryanheise.com>
2023-11-25 19:45:43 +1100

    Add linux support to example

--- audio_service/example/lib/common.dart
+++ audio_service/example/lib/common.dart
@@ -31,10 +31,10 @@ class SeekBar extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _SeekBarState createState() => _SeekBarState();
+  SeekBarState createState() => SeekBarState();
 }
 
-class _SeekBarState extends State<SeekBar> {
+class SeekBarState extends State<SeekBar> {
   double? _dragValue;
   bool _dragging = false;
   late SliderThemeData _sliderThemeData;
